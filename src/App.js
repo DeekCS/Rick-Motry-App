24322c228ec053aba521f3098c001b27d13b1a0b
DeekCS <31720001079@std.bau.edu.jo>
2021-11-28 22:50:19 +0200

    add pagination and search

--- src/App.js
+++ src/App.js
@@ -8,16 +8,37 @@ import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 
 
+
 function App() {
+    let [fetchedData,updateFetchedData] = useState([]);
+    let {info , results} = fetchedData;
+    let [pageNumber, updatePageNumber] = useState(1);
+    let [search, setSearch] = useState("");
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+
+    useEffect(() => {
+        (async function () {
+            let data = await fetch(api).then((res) => res.json());
+            updateFetchedData(data);
+        })();
+    },
+        [api]);
+
   return (
     <div className="App">
      <h1 className="text-center mb-3">Characters</h1>
+        <Search setSearch={setSearch} updatePageNumber={updatePageNumber}/>
       <div className="container">
         <div className="row">
-        {/*  Filter Component*/}
+            {/*{}*/}
           <div className="col-lg-8 col-12">
-            {/* Card Component */}
+              <Card results={results} />
           </div>
+            <Pagination
+                info={info}
+                pageNumber={pageNumber}
+                updatePageNumber={updatePageNumber}
+            />
         </div>
       </div>
     </div>
